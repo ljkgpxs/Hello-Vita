c972fd5303afc5ebbae4db8a42b44cd9f85dbd2c
ljkgpxs <uasoft@yeah.net>
2016-08-05 22:32:18 +0800

    Fix the conflict between one point touch and two

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 ##Hello_Vita
 
 Sample program for PSVita
-
+  
 ##Compiling
 requisites:
 - psp2sdk
 - [libvita2d](https://github.com/xerpi/libvita2d)
-
+  
 ##Usage
-Up : Set CPU Clock + 1 <br />
-Down : Set CPU Clock - 1 <br />
-L : Circle's raid - 10 <br />
-R : Circle's raid + 10 <br />
-Start : Quit program <br />
-Front touch one point : Set Circle's position <br />
-Front touch two points : Change Circle's radius <br />
-
+Up : Set CPU Clock + 1  
+Down : Set CPU Clock - 1  
+L : Circle's raid - 10  
+R : Circle's raid + 10  
+Start : Quit program  
+Front touch two points : Change Circle's radius and position  
+  
 ##TODO
-fix bug s<br />
-optimize gesture recognition<br />
-and more...<br />
+fix bugs  
+and more what I think about...  
--- src/main.c
+++ src/main.c
@@ -24,9 +24,10 @@ int main(int argc, char *argv[])
 	SceCtrlData pad;
 	SceTouchData touch;
 	SceUInt64 lastTime;
+	SceInt64 sampleNum;
 
-	float radi = 10.0f, lastLen = 0.0f, fps = 0.0f, tmp = 0.0f;
-	int freq = 333, x = SCREEN_W / 2.0, y = SCREEN_H / 2.0, n = 1;
+	SceFloat radi = 10.0f, lastLen = 0.0f, fps = 0.0f, tmp = 0.0f;
+	SceInt32 freq = 333, x = SCREEN_W / 2.0, y = SCREEN_H / 2.0;
 
 	vita2d_init();
 	vita2d_set_clear_color(RGBA8(0, 0, 0, 255));
@@ -61,12 +62,13 @@ int main(int argc, char *argv[])
 		if(touch.reportNum == 2) {
 			// Get gesture to change the circle's radius
 			tmp = (float)sqrt(pow(touch.report[0].x - touch.report[1].x, 2.0) + pow(touch.report[0].y - touch.report[1].y, 2.0));
-			if(n == 1) {
+			if(sampleNum == 1) {
 				lastLen = tmp;
 				lastTime = touch.timeStamp;
-				n = 0;
+				sampleNum = 0;
 			} else {
-				if(touch.timeStamp - lastTime < 100000) {
+				if(touch.timeStamp - lastTime < 1E5) {
+					// Time between two samples, and 1E6 is 1 second
 					if(radi + (tmp - lastLen) / 2.0 > 0)
 						radi += (tmp - lastLen) / 2.0;
 					else
@@ -75,15 +77,12 @@ int main(int argc, char *argv[])
 					lastTime = touch.timeStamp;
 				} else {
 					lastLen = 0;
-					n = 1;
+					sampleNum = 1;
 				}
+			x = lerp((abs(touch.report[0].x + touch.report[1].x) / 2.0), 1920, SCREEN_W);
+			y = lerp((abs(touch.report[0].y + touch.report[1].y) / 2.0), 1088, SCREEN_H);
 			}
-		} /* else if(touch.reportNum == 1) {
-			// Front touchscreen is 1920x1088
-			// Get touch point to change circle's position
-			x = lerp(touch.report[0].x, 1920, SCREEN_W);
-			y = lerp(touch.report[0].y, 1088, SCREEN_H);
-		} */
+		}
 
 		vita2d_start_drawing();
 		vita2d_clear_screen();
@@ -95,8 +94,8 @@ int main(int argc, char *argv[])
 
 		vita2d_draw_fill_circle(x, y, abs(radi), RGBA8(0, 255, 255, 255));
 		vita2d_pgf_draw_textf(pgf, 25, 25, RGBA8(255, 255, 255, 255), 1.0f, \
-			"Program By Ljkgpxs lx:%d ly:%d rx:%d ry:%d  %.1f tstamp:%ld status:%ld", pad.lx, pad.ly, pad.rx, pad.ry, tmp, touch.timeStamp, touch.status);
-		vita2d_pgf_draw_textf(pgf, 25, 45, RGBA8(255, 255, 255, 255), 1.0f, " Current CPU clock:%d Bus Clock:%d GPU clock:%d", \
+			"Program By Ljkgpxs lx:%d ly:%d rx:%d ry:%d", pad.lx, pad.ly, pad.rx, pad.ry);
+		vita2d_pgf_draw_textf(pgf, 25, 45, RGBA8(255, 255, 255, 255), 1.0f, " Current Arm Clock:%d Bus Clock:%d GPU Clock:%d", \
 			scePowerGetArmClockFrequency(), scePowerGetBusClockFrequency(), scePowerGetGpuClockFrequency());
 		
 		sceDisplayGetRefreshRate(&fps);
